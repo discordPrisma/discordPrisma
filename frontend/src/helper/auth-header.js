60e96799974312a34d0bbf778abeb4ccd887b358
Avocado <43632131+Awoocado@users.noreply.github.com>
2021-10-07 22:04:39 -0500

    Y esto tampoco

--- frontend/src/helper/auth-header.js
+++
@@ -1,6 +0,0 @@
-export function authHeader () {
-  const user = JSON.parse(localStorage.getItem('user'))
-
-  if (user && user.authdata) return { Authorization: 'Basic ' + user.authdata }
-  else return {}
-}
